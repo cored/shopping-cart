43a5940df2dfb4bd9b659284348d2ffd9c2d1f49
Rafael George <george.rafael@gmail.com>
2010-05-19 18:52:23 -0400

    first commit

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source :rubygems
+
+# Project requirements
+gem 'rake'
+gem 'rack-flash'
+gem 'thin' # or mongrel
+
+# Component requirements
+gem 'activerecord', :require => "active_record"
+gem 'mysql', :require => "mysql"
+
+# Test requirements
+gem 'rspec', :require => "spec", :group => "test"
+gem 'rack-test', :require => 'rack/test', :group => 'test'
+
+# Padrino
+gem 'padrino', "0.9.10"
---
+++ app/controllers/cart.rb
@@ -0,0 +1,27 @@
+ShoppingCart.controllers :cart do
+  # get :index, :map => "/foo/bar" do
+  #   session[:foo] = "bar"
+  #   render 'index'
+  # end
+
+  # get :sample, :map => "/sample/url", :respond_to => [:any, :js] do
+  #   case content_type
+  #     when :js then ...
+  #     else ...
+  # end
+
+  # get :foo, :with => :id do
+  #   "Maps to url '/foo/#{params[:id]}'"
+  # end
+
+  # get "/example" do
+  #   "Hello world!"
+  # end
+
+  get :index, :map => '/' do
+  	session[:cart] = []
+  	@products = ['iPod black', 'iMac', 'iMac RC', 'iPod']
+  	render 'cart/index'
+  end
+
+end
