5bdb2eaf74f773da0bb64771b4a8c3d3a237bda0
Rafael George <george.rafael@gmail.com>
2010-05-24 19:03:35 -0400

    added some finals notes to the README

--- app/controllers/cart.rb
+++ app/controllers/cart.rb
@@ -1,20 +1,17 @@
 ShoppingCart.controllers :cart do
     get :index, :map => '/' do
-        session[:cart] = []
+        session[:cart] = {}
         @products = ['iPod black', 'iMac', 'iMac RC', 'iPod']
         render 'cart/index'
     end
 
     get :add, :map => '/add' do
         product_id = params[:id][-1].to_i
-        cart = session[:cart] || []
-        unless cart[product_id]
-            cart[product_id] = 1
-        else
-            cart[product_id] += 1
-        end
+        cart = session[:cart] || {}
+        cart[product_id] ? cart[product_id] += 1 : cart[product_id] = 1
         session[:cart] = cart
         @products = ['iPod black', 'iMac', 'iMac RC', 'iPod']
+        render 'cart/add'
     end
 
 end
