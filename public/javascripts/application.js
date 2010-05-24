d9bfd5816c403ff7e825f2a59089fb92055fdfb2
Rafael George <george.rafael@gmail.com>
2010-05-24 17:41:35 -0400

    added new drag and drop behaviour with prototype

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,11 @@
 // Put your application scripts here
-for (var i=0; i<3; i++) {
+for (var i=0; i<4; i++) {
     new Draggable('product_'+i, { revert: true });
 }
+
+Droppables.add('cart', {
+    accept: 'products',
+    onDrop: function(element) {
+        $('items').update('Dropped an element'+element.id+'on me');
+    }
+});
