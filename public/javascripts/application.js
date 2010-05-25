c6dc1a1b9349885b17659bc5fcc96af65ac21bee
Rafael George <george.rafael@gmail.com>
2010-05-25 09:25:14 -0400

    improvements to the drag and drop behaviour - added the hover behaviour for the cart - added busy indicator gif - added the last part to the README tutorial

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,9 +3,23 @@ for (var i=0; i<4; i++) {
     new Draggable('product_'+i, { revert: true });
 }
 
+Ajax.Responders.register({
+    onCreate: function() {
+        if($('indicator') && Ajax.activeRequestCount>0)
+            Element.show('indicator');
+    },
+    onComplete: function() {
+        if($('indicator') && Ajax.activeRequestCount==0)
+            Element.hide('indicator');
+    }
+});
+
 Droppables.add('cart', {
     accept: 'products',
     onDrop: function(element) {
         new Ajax.Updater('items', $('shopping_cart').readAttribute('data-url'), { method: 'get', parameters: { id: element.id }});
-    }
+    }, 
+    hoverclass: "cart-active"
 });
+
+
